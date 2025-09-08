32dda7b6651719bd50216ddc7ca11403ded443d7
atmik-prodt <atmiktumane72@gmail.com>
2025-09-08 19:11:20 +0530

    Installations - SpringBoot App start

---
+++ src/test/java/com/java/CarMarket_backend/CarMarketBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.CarMarket_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarMarketBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
