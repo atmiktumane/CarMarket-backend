69572741fb7b09e0e6b49f73f9e84527be373a41
atmik-prodt <atmiktumane72@gmail.com>
2025-09-08 21:31:05 +0530

    Sprind Security - Store Hashed Password in Database

--- src/main/java/com/java/CarMarket_backend/service/impl/UserServiceImpl.java
+++ src/main/java/com/java/CarMarket_backend/service/impl/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.java.CarMarket_backend.service.impl;
 
 import com.java.CarMarket_backend.dto.UserDTO;
+import com.java.CarMarket_backend.exception.EmailAlreadyExistsException;
+import com.java.CarMarket_backend.model.Role;
 import com.java.CarMarket_backend.model.UserModel;
 import com.java.CarMarket_backend.repository.UserRepository;
 import com.java.CarMarket_backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service // This annotation is mandatory for spring to detect it as a bean
@@ -12,13 +15,44 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     @Override
     public UserDTO registerUser(UserDTO userDTO){
-        UserModel user = userDTO.convertToUserModel();
+        // Check if Email already exists
+        if(userRepository.findByEmail(userDTO.getEmail()).isPresent()) {
+            throw new EmailAlreadyExistsException("Email already exists : " + userDTO.getEmail());
+        }
 
+        UserModel user = new UserModel();
+
+        // Set Name & Email
+        user.setName(userDTO.getName());
+        user.setEmail(userDTO.getEmail());
+
+        // Encrypt the password before saving
+        user.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+
+        // Set Role (from request body or default as BUYER
+        if(userDTO.getRole() != null){
+            user.setRole(userDTO.getRole());
+        }else{
+            user.setRole(Role.BUYER); // default role
+        }
+
+        // Save User data in DB
         UserModel savedUser = userRepository.save(user);
 
-        return savedUser.convertToUserDTO();
+        // Prepare Response body
+        UserDTO response = new UserDTO();
+
+        response.setId(savedUser.getId());
+        response.setName(savedUser.getName());
+        response.setEmail(savedUser.getEmail());
+        response.setRole(savedUser.getRole());
+
+        return response;
     }
 
 
