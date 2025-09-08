75aab51880d7fd24d6f788a8d8cb6a61410fcf1f
atmik-prodt <atmiktumane72@gmail.com>
2025-09-09 00:37:09 +0530

    Job Scheduler - Run automatically every minute to check for Expired OTPs

--- src/main/java/com/java/CarMarket_backend/repository/OtpRepository.java
+++ src/main/java/com/java/CarMarket_backend/repository/OtpRepository.java
@@ -7,5 +7,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface OtpRepository extends MongoRepository<OtpModel, String> {
+
+    // Job Scheduler - to delete expired OTPs after every 5 minutes
     List<OtpModel> findByCreationTimeBefore(LocalDateTime expriyTime);
 }
